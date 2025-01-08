7217d8ed9aa85e3236374898a980c5dcfbdba851
Sidharth Panda <sidharth.panda@hotmail.com>
2025-01-08 15:45:52 +0530

    Need to fix

--- First_file.py
+++ First_file.py
@@ -1,1 +1,26 @@
-print("Hello world")
+print("Welcome to first program")
+print("***************************************************")
+
+a=float(input("Enter first value: "))
+b= float(input("Ebter second value: "))
+
+print("Enter any option from below: ")
+print("***************************************************")
+print("Press 1 for addition")
+print("Press 2 for subtraction")
+print("Press 3 for multiplication")
+print("Press 4 for division")
+print("***************************************************")
+
+c=input()
+
+if c==1:
+    print("addition of two value is: {}".format(a+b))
+elif c==2:
+    print("subtraction of two value is: {}".format(a - b))
+elif c==3:
+    print("multiplication of two value is: {}".format(a * b))
+elif c==4:
+    print("division of two value is: {}".format(a / b))
+else:
+    print("Enter correct option.")
